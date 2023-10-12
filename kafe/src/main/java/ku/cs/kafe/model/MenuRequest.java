5a31fb1e01870f9fdcb4f70532acdcb947cfcd38
benny200iq <thanaphat.tan@ku.th>
2023-10-12 16:32:04 +0700

    first commit

---
+++ kafe/src/main/java/ku/cs/kafe/model/MenuRequest.java
@@ -0,0 +1,16 @@
+
+package ku.cs.kafe.model;
+
+
+import lombok.Data;
+
+import java.util.UUID;
+
+
+@Data
+public class MenuRequest {
+    private String name;
+    private double price;
+    private UUID categoryId;
+}
+
